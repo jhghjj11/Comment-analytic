39dad95b2631911aba5ea8b88e8321bb4920177e
jhghjj11 <57930789+jhghjj11@users.noreply.github.com>
2020-08-02 14:07:37 +0800

    Add files via upload

--- Comments analysis.py
+++ Comments analysis.py
@@ -4,11 +4,23 @@ with open('reviews.txt', 'r') as f :
     for line in f :
         data.append(line)
         count = count + 1
-        if count % 1000 == 0 :
+        if count % 10000 == 0 :
             print(count)
-    print('The file total number of line:', count)
+print('The file total number of line:', count)
+
+sum = 0
+for d in data:
+    sum = sum + len(d)
+    avg = sum / len(data)
+print('The average length is', avg)
 
 
-print(len(data))
-print('--------------------------------------------------------------------------------------------------')
+
+
+
+
+
+
 print(data[0])
+print('--------------------------------------------------------------------------------------------------')
+print(data[1])
