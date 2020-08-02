ef39d998b98b5f2ac379d7994e3558899022a782
jhghjj11 <57930789+jhghjj11@users.noreply.github.com>
2020-08-02 13:44:22 +0800

    Add files via upload

---
+++ Comments analysis.py
@@ -0,0 +1,12 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f :
+    for line in f :
+        data.append(line)
+        count = count + 1
+        if count % 1000 == 0 :
+            print(count)
+
+
+print(len(data))
+print(data[0])
